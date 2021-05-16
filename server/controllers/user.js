d3f5c20496375bd978badbdf132c4575baf3a6ef
Daniel Boesch <you@example.com>
2021-05-16 17:08:50 -0600

    fixed some bugs

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,36 +5,35 @@ module.exports = {
     register: async (req, res) => {
         const db = req.app.get('db')
         const {username, password} = req.body
-        const {profile_pic} = (`https://robohash.org/${username}.png`)
 
-        const [checkUsername] = await db.auth.find_user_by_username(username)
-        if (checkUsername){
+        const result = await db.user.find_user_by_username([username])
+        const existingUser = result[0]
+        if (existingUser){
             return res.status(409).send("Username already exists")
         }
 
-        const salt = bcrypt.genSaltSync(15)
+        const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
-        const [user] = await db.auth.create_user(username, hash, profile_pic)
-        delete user.password
-        req.session.user = user
+        const registeredUser = await db.user.create_user([username, hash, `https://robohash.org/${username}.png`])
+        const user = registeredUser[0]
+        req.session.user={id: user.id, username: user.username, profile_pic: user.profile_pic}
         return res.status(200).send(req.session.user)
+        
     },
 
     login: async (req, res) => {
-        const db = req.app.get('db')
-        const {username, password} = req.body
-        const {profile_pic} = (`https://robohash.org/${username}.png`)
+        const {username, password} = req.body 
+        const foundUser = await req.app.get('db').user.find_user_by_username({username})
+        const user = foundUser[0]
 
-        const [user] = await db.auth.find_user_by_username(username)
         if(!user){
             return res.status(404).send("User was not found.")
         }
-        const isAuthenticated = bcrypt.compareSync(password, user.password)
+        const isAuthenticated = bcrypt.compareSync(password, user.hash)
         if (!isAuthenticated){
             return res.status(403).send("Wrong Password")
         }
-        delete user.password
-        req.session.user = user
+        req.session.user = {id:user.id, username: user.username, profile_pic: `https://robohash.org/${username}.png`}
         return res.status(200).send(req.session.user)
 
     },
